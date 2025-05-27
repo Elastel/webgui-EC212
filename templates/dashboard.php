aa84ecb1b8d994fcf56a7248efe7e079a661e5e4
elastel <elastel@example.com>
2025-05-27 15:24:25 +0800

    webgui: Fix lte info show
    Signed-off-by: elastel <elastel@example.com>

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -127,7 +127,7 @@
                     </br>
                   <?php } ?>
                   <?php endforeach ?>
-                  <?php if ($lteInfo['enabled'] == "1") { ?>
+                  <?php if (isset($lteInfo['iccid']) && strlen($lteInfo['iccid']) > 0) { ?>
                     <h4 class="card-title"><?php echo _("LTE Network Status"); ?></h4>
                     <div class="row ml-1">
                       <div class="col-sm">
