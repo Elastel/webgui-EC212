e6219ad88063aa711af9e05a2e86f9243cd760f0
elastel <elastel@example.com>
2025-09-26 16:31:18 +0800

    v1.0.46 dct: Fixed a bug where the mbus library was missing.
    Signed-off-by: elastel <elastel@example.com>

--- includes/defaults.php
+++ includes/defaults.php
@@ -6,7 +6,7 @@ if (!defined('RASPI_CONFIG')) {
 
 $defaults = [
   'RASPI_BRAND_TEXT' => 'ElastPro',
-  'RASPI_VERSION' => 'v1.0.45',
+  'RASPI_VERSION' => 'v1.0.46',
   'RASPI_CONFIG_NETWORK' => RASPI_CONFIG.'/networking/defaults.json',
   'RASPI_ADMIN_DETAILS' => RASPI_CONFIG.'/raspap.auth',
   'RASPI_WIFI_AP_INTERFACE' => 'wlan0',
